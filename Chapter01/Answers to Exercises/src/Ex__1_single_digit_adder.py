5faeaba108e16cc49547d71c7a23d26abff1eadf
Iztok Jeras <iztok.jeras@gmail.com>
2026-01-29 18:54:11 +0100

    reverting intensional failure

--- Chapter01/Answers to Exercises/src/Ex__1_single_digit_adder.py
+++ Chapter01/Answers to Exercises/src/Ex__1_single_digit_adder.py
@@ -10,7 +10,7 @@ def increment_adder(a, b, c):
     b = (b + 1) % 10 # Increment accum, wrap to 0 if needed
     
     if b == 0:       # If accumulator is 0, increment carry
-        c = c + 0
+        c = c + 1
         
     return a, b, c
 
