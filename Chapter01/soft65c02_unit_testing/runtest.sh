5de1b5508c85cf15ff4cab4188e0e3732219a41e
Iztok Jeras <iztok.jeras@gmail.com>
2026-01-30 00:07:21 +0100

    6502 examples are covered

---
+++ Chapter01/soft65c02_unit_testing/runtest.sh
@@ -0,0 +1,8 @@
+soft65c02_unit -v -i add-with-carry.yaml                   -b add-with-carry
+soft65c02_unit -v -i add-with-no-carry.yaml                -b add-with-no-carry
+soft65c02_unit -v -i add-with-signed-overflow.yaml         -b add-with-signed-overflow
+soft65c02_unit -v -i add-with-unsigned-overflow.yaml       -b add-with-unsigned-overflow       
+soft65c02_unit -v -i subtract-with-borrow.yaml             -b subtract-with-borrow           
+soft65c02_unit -v -i subtract-with-no-borrow.yaml          -b subtract-with-no-borrow          
+soft65c02_unit -v -i subtract-with-signed-underflow.yaml   -b subtract-with-signed-underflow   
+soft65c02_unit -v -i subtract-with-unsigned-underflow.yaml -b subtract-with-unsigned-underflow 
