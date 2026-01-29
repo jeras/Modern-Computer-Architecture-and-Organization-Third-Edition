5ce750aa42ad2b157d2c6a7021f13cc95fe995c6
Iztok Jeras <iztok.jeras@gmail.com>
2026-01-29 22:56:56 +0100

    removed STP instruction added in crt0.s

--- Chapter01/soft65c02_unit_testing/base_configs/crt0.s
+++ Chapter01/soft65c02_unit_testing/base_configs/crt0.s
@@ -4,7 +4,7 @@
 ; this is the beginning of the app, if anything directly calls it, we halt
 .segment "STARTUP"
 halt:
-        .byte   $db         ; STP in 65c02 emulator
+;        .byte   $db         ; STP in 65c02 emulator
 
 ; if "init" is called in soft65c02_tester, call halt and stop the emulator
 .segment "V_RESET"
