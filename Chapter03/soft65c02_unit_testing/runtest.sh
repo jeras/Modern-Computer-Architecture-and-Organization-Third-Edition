34d3dcab0570866f11c3b00b1684b935ca16f34e
Iztok Jeras <iztok.jeras@gmail.com>
2026-02-13 08:51:58 +0100

    Chapter 03: some 6502 testcases

---
+++ Chapter03/soft65c02_unit_testing/runtest.sh
@@ -0,0 +1,9 @@
+soft65c02_unit -v -i absolute_addressing_mode.yaml            -b absolute_addressing_mode
+soft65c02_unit -v -i absolute_indexed_addressing_mode.yaml    -b absolute_indexed_addressing_mode
+soft65c02_unit -v -i immediate_addressing_mode.yaml           -b immediate_addressing_mode
+soft65c02_unit -v -i indirect_indexed_addressing_mode.yaml    -b indirect_indexed_addressing_mode
+soft65c02_unit -v -i looping_absolute_indexed_addressing.yaml -b looping_absolute_indexed_addressing
+
+soft65c02_unit -v -i Ex__4_checksum_alg.yaml                  -b Ex__4_checksum_alg
+soft65c02_unit -v -i Ex__5_checksum_subroutine.yaml           -b Ex__5_checksum_subroutine
+soft65c02_unit -v -i Ex__6_checksum_tests.yaml                -b Ex__6_checksum_tests
